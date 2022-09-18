67d34647f3a00e5645991be3033acdd8d8b9be64
Captain Unknown <captainunknown7@gmail.com>
2022-09-19 01:16:17 +0500

    🛠️ Major Fixes

--- index.js
+++ index.js
@@ -21,102 +21,95 @@ const clientSecret = process.env.PAYPAL_CLIENT_SECRET;
 const Environment = process.env.NODE_ENV === "production" ? paypal.core.LiveEnvironment : paypal.core.SandboxEnvironment;
 const paypalClient = new paypal.core.PayPalHttpClient( new Environment(clientID, clientSecret));
 
-const getExchangeRate = async (req, res) => {
-  let exchangeRate;
-  let rawResponse;
-
-  let data = JSON.stringify("{\"currency\":\"USD\",\"code\":\"DERO\",\"meta\":false}");
-
-  let config = {
-    method: 'post',
-    url: 'https://api.livecoinwatch.com/coins/single',
+var exchangeRate;
+function getExchangeRate () {
+  var config = {
+    method: 'get',
+    url: 'https://api.coinranking.com/v2/coin/9jgCbgZ_J9mj-',
     headers: {
-      'X-Api-Key': process.env.LCW_API_KEY,
-      'Content-Type': 'application/json'
-    },
-    data : data
+      'x-access-token': process.env.CR_API_KEY
+    }
   };
 
   axios(config)
   .then(function (response) {
-    rawResponse = data;
-
-    const content = rawResponse;
-    exchangeRate = content.rate;
-    console.log(JSON.stringify(response.data));
+    console.log(response.data.data.coin.price);
+    exchangeRate = parseFloat((parseFloat(response.data.data.coin.price)).toFixed(2));
+    console.log(exchangeRate);
   })
   .catch(function (error) {
-    res.status(503).json({ error: "Failed to get current Exchange rates." });
+    console.log(error);
+    res.status(500).json({ error: 'Failed to get exchange rate. Please try again later.' });
   });
-
-  console.log();
   return exchangeRate;
 }
 
-const storeItems = new Map([
-  [1, { price: 3.00, name: "DERO" }], //getExchangeRate() should return total price & quantity must be 1
-])
-
 app.post("/create-order", async (req, res) => {
-  //const wallet = req.body.items.wallet
-  const request = new paypal.orders.OrdersCreateRequest()
-  const total = req.body.items.reduce((sum, item) => {
-    return sum + storeItems.get(item.id).price * item.quantity
-  }, 0)
-  request.prefer("return=representation")
-  request.requestBody({
-    intent: "CAPTURE",
-    purchase_units: [
-      {
-        amount: {
-          currency_code: "USD",
-          value: total,
-          breakdown: {
-            item_total: {
-              currency_code: "USD",
-              value: total,
+  getExchangeRate();
+  setTimeout(async function(){
+    const storeItems = new Map([
+      [1, { price: exchangeRate, name: "DERO" }],
+    ]);
+    const request = new paypal.orders.OrdersCreateRequest()
+    const total = req.body.items.reduce((sum, item) => {
+      return sum + storeItems.get(item.id).price * item.quantity
+    }, 0)
+    request.prefer("return=representation")
+    request.requestBody({
+      intent: "CAPTURE",
+      purchase_units: [
+        {
+          amount: {
+            currency_code: "USD",
+            value: total,
+            breakdown: {
+              item_total: {
+                currency_code: "USD",
+                value: total,
+              },
             },
           },
+          items: req.body.items.map(item => {
+            const storeItem = storeItems.get(item.id)
+            return {
+              name: storeItem.name,
+              unit_amount: {
+                currency_code: "USD",
+                value: storeItem.price,
+              },
+              quantity: item.quantity,
+            }
+          }),
         },
-        items: req.body.items.map(item => {
-          const storeItem = storeItems.get(item.id)
-          return {
-            name: storeItem.name,
-            unit_amount: {
-              currency_code: "USD",
-              value: storeItem.price,
-            },
-            quantity: item.quantity, //should be 1
-          }
-        }),
-      },
-    ],
-  })
-
-  try {
-    const order = await paypalClient.execute(request)
-    res.json({ id: order.result.id })
-  } catch (e) {
-    res.status(500).json({ error: e.message })
-  }
+      ],
+    })
+  
+    try {
+      const order = await paypalClient.execute(request)
+      res.json({ id: order.result.id })
+    } catch (e) {
+      res.status(500).json({ error: e.message })
+    }
+ }, 1000);
 })
 
 app.post("/capture-order", async (req, res) => {
   const request = new paypal.orders.OrdersCaptureRequest(req.body.orderID);
   request.requestBody({});
+  
+  const wallet = req.body.wallet;
 
   try {
-    const capture = await paypalClient.execute(request);
+    const capture = await paypalClient.execute(request);    
+    console.log(`Capture: ${JSON.stringify(capture)}`);
 
-    console.log(`Response: ${JSON.stringify(response)}`);
-    console.log(`Capture: ${JSON.stringify(response.result)}`);
-
-    console.log(capture);
+    const quantity = parseFloat((capture.result.purchase_units[0].payments.captures[0].amount.value / exchangeRate).toFixed(5));
+    console.log('Quantity: ' + quantity);
+    console.log('Gross Value: ' + capture.result.purchase_units[0].payments.captures[0].amount.value);
+    console.log('Exchange Rate: ' + exchangeRate);
     
-    //const paymentInfo = capture.result.purchase_units[0].payments.captures[0];
-    //const DEROAmount = paymentInfo.amount.value;
-
     const client = new MongoClient(process.env.DB_URL, { useUnifiedTopology: true });
+
     try {
       await client.connect();
       await transactionDBHandler(client, capture, quantity, wallet);
@@ -136,14 +129,14 @@ app.listen(PORT, () => {
 
 const transactionDBHandler = async (client, capture, quantity, wallet) => {
   const result = await client.db('BuyDERO').collection('Purchase').insertOne(capture);
-  console.log(`New TX Created: ${result.insertedId}`);
+  console.log(`User Checkout saved: ${result.insertedId}`);
   
   if (getVaultBalance() < quantity) {
     res.status(500).json({ error: 'Our vault wallet is waiting for a refill process. Your DEROs will be manually dispatched as soon as possible, We\'re sorry for any inconvenience caused.' });
-    return 1
+  } else {
+    await releaseDERO(quantity, wallet);
+    console.log(`Released ${quantity} DEROs to ${wallet}`);
   }
-
-  await releaseDERO(quantity, wallet);
 }
 
 const getVaultBalance = async () => {
@@ -183,7 +176,7 @@ const releaseDERO = async (quantity, wallet) => {
     "params": {
       "scid": "00000000000000000000000000000000",
       "destination": wallet,
-      "amount": quantity
+      "amount": quantity * 100000
     }
   });
 
